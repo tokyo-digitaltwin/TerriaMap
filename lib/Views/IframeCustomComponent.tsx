f0b321482abd9719ef1ab753ff264fca2f42476f
Yusuke Kiuchi <4221178-yusuke.kiuchi@users.noreply.gitlab.com>
2021-07-07 17:57:35 +0900

    Disallow non-youtube iframes

--- lib/Views/IframeCustomComponent.tsx
+++ lib/Views/IframeCustomComponent.tsx
@@ -4,6 +4,17 @@ import CustomComponent, {
 } from "terriajs/lib/ReactViews/Custom/CustomComponent";
 import React, { ReactElement } from "react";
 
+function isAllowedSrc(src: string): boolean {
+  // YouTube の iframe だけを許可。
+  if (
+    src.startsWith("https://www.youtube-nocookie.com/") ||
+    src.startsWith("https://www.youtube.com/")
+  ) {
+    return true;
+  }
+  return false;
+}
+
 export default class IframeCutromComponent extends CustomComponent {
   get name() {
     return "iframe";
@@ -25,6 +36,20 @@ export default class IframeCutromComponent extends CustomComponent {
     children: ReactElement[],
     index: number
   ): ReactElement | undefined {
-    return <iframe src={node.attribs?.src} />;
+    const src = node.attribs?.src;
+    if (!isAllowedSrc(src)) {
+      return null;
+    }
+    return (
+      <iframe
+        width={node.attribs?.width}
+        height={node.attribs?.height}
+        src={node.attribs?.src}
+        title={node.attribs?.title}
+        frameBorder={node.attribs?.frameborder}
+        allow={node.attribs?.allow}
+        allowFullScreen={node.attribs?.allowfullscreen != null}
+      />
+    );
   }
 }
